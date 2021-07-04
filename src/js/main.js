bd3878e91f5fea259517648eb6f1540702aa5d0a
Jesus98Sotelo <jesus_sotelo_98@hotmail.com>
2021-07-04 12:38:57 -0500

    mimificamos los archivos de .js y .css

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import '../css/main.css';
+
 document.getElementById('icon__menu').addEventListener('click', () => {
   document.getElementById('nav__menu').classList.toggle('show');
   document.getElementById('icon__menu').classList.toggle('fa-ellipsis-v')
